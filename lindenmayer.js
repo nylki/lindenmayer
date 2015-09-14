232fdd319bfd3efe1e7b72a2add36981d7e480cd
Tom Brewe <tom@tombr.de>
2015-09-14 13:40:02 +0200

    Update lindenmayer.js

--- lindenmayer.js
+++ lindenmayer.js
@@ -95,6 +95,11 @@ class LSystem {
 
 }
 
- if(module !== undefined) exports.LSystem = LSystem
+// if in node export LSystem, otherwise don't attempt to
+try {
+    exports.LSystem = LSystem
+}
+catch(err) {
 
-console.log('loaded lindenmayer.js')
+}
+
