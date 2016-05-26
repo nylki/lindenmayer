78eed54c5ff6dac7c4ede716ae11ebe648580d52
Tom <tombrwe@gmail.com>
2016-05-26 22:15:44 +0200

    added lsystem builder with threejs backend

--- examples/webworker/worker.js
+++ examples/webworker/worker.js
@@ -7,11 +7,15 @@ onmessage = function(e) {
   // wait a few ms to start thread, to be able to cancel old tasks
   clearTimeout(timeout);
   timeout = setTimeout(function() {
-      console.log('received message from main', e. data);
       lsystem.setAxiom(e.data.axiom);
-      console.log('succesfully set the axiom');
-      lsystem.setProductions(e.data.productions);
-      console.log('succesfully set the productions');
+      for (let p of e.data.productions) {
+        if(p.parsejs) {
+          lsystem.setProduction(p.from, eval(p.to));
+        } else {
+          lsystem.setProduction(p.from, p.to);
+        }
+      }
+      console.log(e.data);
       lsystem.iterate(parseInt(e.data.iterations));
       console.log('iterated!');
       postMessage({
