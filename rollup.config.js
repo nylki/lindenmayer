6d725b265a1f7b07c6b441b819f46358ac38aa73
Tom <tombrwe@gmail.com>
2017-03-31 12:48:09 +0200

    Add build script for minification for all three build variants.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import babel from 'rollup-plugin-babel';
-// import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
+
+let minifyEnv = process.env.minify || false;
 
 export default {
   entry: 'lindenmayer.js',
   format: 'cjs',
-  plugins: [ babel() /*uglify()*/ ],
+  plugins: [ babel(), minifyEnv ? uglify({}, minify) : {} ],
   targets: [
-    { dest: 'dist/lindenmayer.js', format: 'cjs' },
-    { dest: 'dist/lindenmayer.browser.js', format: 'iife' },
-    { dest: 'dist/lindenmayer.es.js', format: 'es' },
+    { dest: minifyEnv ? 'dist/lindenmayer.min.js' : 'dist/lindenmayer.js', format: 'cjs' },
+    { dest: minifyEnv ? 'dist/lindenmayer.browser.min.js' : 'dist/lindenmayer.browser.js', format: 'iife' },
+    { dest: minifyEnv ? 'dist/lindenmayer.es.min.js' : 'dist/lindenmayer.es.js', format: 'es' },
   ],
   moduleName: 'LSystem'
 };
